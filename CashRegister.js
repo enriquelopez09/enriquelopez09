2c121df314cbf62b09c70d924ebbb94c2fe4e05a
Enrique López Trinidad <70590461+enriquelopez09@users.noreply.github.com>
2020-09-18 12:54:15 -0500

    Update CashRegister.js

--- CashRegister.js
+++ CashRegister.js
@@ -1,16 +1,17 @@
 function checkCashRegister(price, cash, cid) {
   let funds=0.0, surplus=cash-price, refund=[];
   const coins = [["ONE HUNDRED", 100],["TWENTY", 20],["TEN", 10],["FIVE", 5],["ONE", 1],["QUARTER", 0.25],["DIME", 0.1],["NICKEL", 0.05],["PENNY", 0.01]];
-	for (var i = 0; i < 9; i++) funds += cid[i][1];
+  const ln = cid.length;
+	for (var i = 0; i < ln; i++) funds += cid[i][1];
   if(funds>surplus){
-    for (var i = 0; i < 9; i++){
+    for (var i = 0; i < ln; i++){
       var payment=0.0;
       while (surplus >= coins[i][1]) {
-        if (cid[(cid.length - 1) - i][1] == 0) break;
+        if (cid[(ln - 1) - i][1] == 0) break;
         payment += coins[i][1];
         surplus = surplus.toFixed(2);
         surplus -= coins[i][1];
-        cid[8 - i][1] -= coins[i][1];
+        cid[ln - 1 - i][1] -= coins[i][1];
       }
       if (payment != 0)refund.push([coins[i][0], payment]);
     }
